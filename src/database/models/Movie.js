8689d29d45639abeab804885b09d49ca63514919
Demetrius Macêdo <macedo.demetrius@gmail.com>
2022-11-22 21:25:34 -0300

    primeir commit

---
+++ src/database/models/Movie.js
@@ -0,0 +1,57 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Movie'; //isto deveria estar no singular
+    let cols = {
+        id: {
+            type: dataTypes.BIGINT(10).UNSIGNED,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        // created_at: dataTypes.TIMESTAMP,
+        // updated_at: dataTypes.TIMESTAMP,
+        title: {
+            type: dataTypes.STRING(500),
+            allowNull: false
+        },
+        rating: {
+            type: dataTypes.DECIMAL(3, 1).UNSIGNED,
+            allowNull: false
+        },
+        awards: {
+            type: dataTypes.BIGINT(10).UNSIGNED,
+            allowNull: false
+        },
+        release_date: {
+            type: dataTypes.DATEONLY,
+            allowNull: false
+        },
+        length: dataTypes.BIGINT(10),
+        genre_id: dataTypes.BIGINT(10)
+    };
+    let config = {
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: false
+    }
+    const Movie = sequelize.define(alias,cols,config);
+
+    //Aqui você tem que fazer o necessário para criar as relações com os modelos (Genre - Actor)
+    Movie.associate = function(modelos){
+        Movie.belongsTo(modelos.Genre, {
+            as: "Genre",
+            foreignKey: "genre_id"
+        });
+    }
+
+    Movie.associate = function(modelos){
+        Movie.belongsToMany(modelos.Actor, {
+            as: "Actor",
+            through: "actor_movie",
+            foreignKey: "movie_id",
+            otherKey: "actor_id"
+        })
+    }
+
+    return Movie
+};
